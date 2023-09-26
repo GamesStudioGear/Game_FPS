206edf7529cf13b9f7f697418d5025231c7c7cab
GamesStudioGear <122485697+GamesStudioGear@users.noreply.github.com>
2023-09-26 20:54:58 -0300

    Aula 03
    Aula 03

--- FPSGun.cs
+++ FPSGun.cs
@@ -7,15 +7,20 @@ public class FPSGun : MonoBehaviour
     [SerializeField]
     public GunType gunType;
     public string gunName;
+    public float recoilForce;
     public int ammunition;
     public int maximumAmmo;
     public int ammunitionToReload;
+    public Transform firePoint;
+    public GameObject muzzeEffect;
+    public AudioSource fireSound;
 
     [HideInInspector]
     public Animator animator;
 
     [HideInInspector]
     public GameObject model;
+
 
     void Awake()
     {
--- FPSGunController.cs
+++ FPSGunController.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class FPSGunController : MonoBehaviour
 {
     public FPSGun[] inventory;
     public FPSGun currentWeapon;
     private FPSAnimations animations;
+    public GameObject cameraContainer;
+    public string wallTag, floorTag, enemyTag, waterTag;
+    public GameObject wallHole, floorHole;
+    public GameObject wallImpact, floorImpact, waterImpact, enemyImpact;
 
     void Start()
     {
@@ -45,10 +51,41 @@ public class FPSGunController : MonoBehaviour
 
     void FireSemiAutomatic()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
+        {
             animations.ToFire();
+            cameraContainer.transform.position = cameraContainer.transform.position - (cameraContainer.transform.forward * currentWeapon.recoilForce);
 
-        //implementar tiro semi automatico
+            currentWeapon.fireSound.Play();
+
+            RaycastHit hit;
+            var destiny = Camera.main.transform.position + Camera.main.transform.forward * 1000;
+
+            var collided = Physics.Raycast(currentWeapon.firePoint.position, destiny, out hit, 1000);
+            Instantiate(currentWeapon.muzzeEffect, currentWeapon.firePoint);
+            if (collided)
+            {
+                if (hit.transform.tag == enemyTag)
+                {
+                    Instantiate(enemyImpact, hit.point, enemyImpact.transform.rotation);
+                    //implementar dano
+                }
+                else if (hit.transform.tag == wallTag)
+                {
+                    Instantiate(wallImpact, hit.point, wallImpact.transform.rotation);
+                    Instantiate(wallHole, hit.point, Quaternion.LookRotation(hit.normal));
+                }
+                else if (hit.transform.tag == floorTag)
+                {
+                    Instantiate(floorImpact, hit.point, floorImpact.transform.rotation);
+                    Instantiate(floorHole, hit.point, Quaternion.LookRotation(hit.normal));
+                }
+                else if (hit.transform.tag == waterTag)
+                {
+                    Instantiate(waterImpact, hit.point, waterImpact.transform.rotation);
+                }
+            }
+        }      
     }
 
     void Reload()
@@ -70,5 +107,6 @@ public class FPSGunController : MonoBehaviour
         currentWeapon.model.SetActive(true);
         currentWeapon.animator.Play("get");
     }
+
 }
 
